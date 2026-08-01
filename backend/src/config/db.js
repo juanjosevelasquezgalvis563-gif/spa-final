16b9242004e164917054de2640a42d72e02a9ad9
juan jose <juanjosevelasquezgalvis663@gmail.com>
2026-08-01 14:23:52 -0500

    configuracion de la db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,14 @@
 import dotenv from "dotenv";
 import mysql from "mysql2";
 
+dotenv.config();
+
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'otra'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
 });
 
 export default db;
