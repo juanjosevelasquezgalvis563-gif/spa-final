ab67047bbb18cc528d30c4c8292f5353c6e06a3b
juan jose <juanjosevelasquezgalvis663@gmail.com>
2026-08-11 17:05:02 -0500

    funcionalidad de inicio de sesion de usuarios

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -16,6 +16,7 @@ export function Login() {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
+          
         },
         body: JSON.stringify({ email, password }),
       });
