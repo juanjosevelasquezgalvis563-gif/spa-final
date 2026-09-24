7f85d49c08374576bd638f5dd36398c46bc8cd31
juan jose <juanjosevelasquezgalvis663@gmail.com>
2026-09-23 21:47:12 -0500

    nuevo middleware para login con cookies

--- backend/src/middlewares/middleware.js
+++ backend/src/middlewares/middleware.js
@@ -1,20 +1,17 @@
 import jwt from 'jsonwebtoken'
 
 export function middleware(req,res,next){
-    const authHeader = req.headers.authorization;
-    if(!authHeader){
+    const token = req.cookies.token;
+    if(!token){
         return res.status(401).json({message: 'sin token'})
     }
-    const token = authHeader.split(' ')[1];
-    if(!token){
-        return res.status(401).json({message: 'token invalido'})
-    }
+
     try{
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         req.user = decoded;
         next();
 
     }catch(error){
-         return res.status(500).json({message: 'token invalido o expirado'})
+         return res.status(401).json({message: 'token invalido o expirado'})
     }
 }
