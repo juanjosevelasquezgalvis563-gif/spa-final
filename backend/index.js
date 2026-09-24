510bb4c56995d8dab7ab45837938b4bc4c4ca2f0
juan jose <juanjosevelasquezgalvis663@gmail.com>
2026-09-23 21:56:51 -0500

    configuracion de entorno de mi proyecto

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,44 @@
 import dotenv from "dotenv";
+import './src/models/associations.js';
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
 import authRoutes from "./src/routes/routes.js";
-
+import db from "./src/config/db.js";
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
-app.use(cors());
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+
+app.use(cookieParser());
 app.use(express.json());
 
 app.get("/health", (req, res) => {
- res.json({ ok: true, message: "Backend funcionando correctamente" });
+    res.json({
+        ok: true,
+        message: "Backend funcionando correctamente"
+    });
 });
+
 app.use("/inser", authRoutes);
 
+try {
+    await db.authenticate();
+    console.log("Base de datos conectada con Sequelize");
 
-app.listen(PORT, () => {
- console.log(`Servidor backend activo en http://localhost:${PORT}`);
-});
+    app.listen(PORT, () => {
+        console.log(`Servidor backend activo en http://localhost:${PORT}`);
+    });
 
-
+} catch (error) {
+    console.error("Error al conectar con la base de datos:", error);
+}
